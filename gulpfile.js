999842995f4b14042cf390eeb4f3597fbfdbf4b2
Nadine <nadinecoelho9@gmail.com>
2020-06-29 22:23:31 -0300

    new task html and instaled gulp watch

--- gulpfile.js
+++ gulpfile.js
@@ -3,3 +3,7 @@ var gulp = require("gulp");
 gulp.task("default", function () {
   console.log("Hooray - you created a Gulp task");
 });
+
+gulp.task("html", function () {
+  console.log("Imagine something useful being done to your HTML");
+});
