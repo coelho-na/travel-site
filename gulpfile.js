11b7fe4f05eec9c5679b46dcac261c86e9b3f0ce
Nadine <nadinecoelho9@gmail.com>
2020-07-01 23:31:15 -0300

    starting to  styling the project with Css and file architecture

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,8 @@ var gulp = require("gulp"),
   postcss = require("gulp-postcss"),
   autoprefixer = require("autoprefixer"),
   cssvars = require("postcss-simple-vars"),
-  nested = require("postcss-nested");
+  nested = require("postcss-nested"),
+  cssImport = require("postcss-import");
 
 gulp.task("default", function () {
   console.log("Hooray - you created a Gulp task");
@@ -16,7 +17,7 @@ gulp.task("html", function () {
 gulp.task("styles", function () {
   return gulp
     .src("./app/assets/styles/styles.css")
-    .pipe(postcss([cssvars, nested, autoprefixer]))
+    .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
     .pipe(gulp.dest("./app/temp/styles"));
 });
 
