f9387719ee9837cd2b69f6f810fe34a30c969e81
Nadine <nadinecoelho9@gmail.com>
2020-07-01 22:38:02 -0300

    Postcss Simple Vars and Nested instaled

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,9 @@
 var gulp = require("gulp"),
-  watch = require("gulp-watch");
+  watch = require("gulp-watch"),
+  postcss = require("gulp-postcss"),
+  autoprefixer = require("autoprefixer"),
+  cssvars = require("postcss-simple-vars"),
+  nested = require("postcss-nested");
 
 gulp.task("default", function () {
   console.log("Hooray - you created a Gulp task");
@@ -12,6 +16,7 @@ gulp.task("html", function () {
 gulp.task("styles", function () {
   return gulp
     .src("./app/assets/styles/styles.css")
+    .pipe(postcss([cssvars, nested, autoprefixer]))
     .pipe(gulp.dest("./app/temp/styles"));
 });
 
