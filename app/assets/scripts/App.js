76ba775dd9cedb2db83785dfaad01a1db195fcfc
Nadine <nadinecoelho9@gmail.com>
2020-07-30 22:02:37 -0300

    Completed the header to desktop view and learn about webpack

---
+++ app/assets/scripts/App.js
@@ -0,0 +1,8 @@
+var $ = require("jquery");
+var Person = require("./modules/Person");
+
+var john = new Person("John Doe", "blue");
+john.greet();
+
+var jane = new Person("Jane Smith", "green");
+jane.greet();
