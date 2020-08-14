ded55fd8167ba8e91a5ac6c675b08728284e1259
Nadine <nadinecoelho9@gmail.com>
2020-08-13 21:03:45 -0300

    Added browser support fallbacks.

--- gulp/tasks/sprites.js
+++ gulp/tasks/sprites.js
@@ -1,11 +1,24 @@
 var gulp = require("gulp"),
   svgSprite = require("gulp-svg-sprite"),
   rename = require("gulp-rename"),
-  del = require("del");
+  del = require("del"),
+  svg2png = require("gulp-svg2png");
 
 var config = {
+  shape: {
+    spacing: {
+      padding: 1,
+    },
+  },
   mode: {
     css: {
+      variables: {
+        replaceSvgWithPng: function () {
+          return function (sprite, render) {
+            return render(sprite).split(".svg").join(".png");
+          };
+        },
+      },
       sprite: "sprite.svg",
       render: {
         css: {
@@ -27,9 +40,16 @@ gulp.task("createSprite", ["beginClean"], function () {
     .pipe(gulp.dest("./app/temp/sprite/"));
 });
 
-gulp.task("copySpriteGraphic", ["createSprite"], function () {
+gulp.task("createPngCopy", ["createSprite"], function () {
   return gulp
-    .src("./app/temp/sprite/css/**")
+    .src("./app/temp/sprite/css/*.svg")
+    .pipe(svg2png())
+    .pipe(gulp.dest("./app/temp/sprite/css"));
+});
+
+gulp.task("copySpriteGraphic", ["createPngCopy"], function () {
+  return gulp
+    .src("./app/temp/sprite/css/**/*.{svg,png}")
     .pipe(gulp.dest("./app/assets/images/sprites"));
 });
 
@@ -47,6 +67,7 @@ gulp.task("endClean", ["copySpriteGraphic", "copySpriteCSS"], function () {
 gulp.task("icons", [
   "beginClean",
   "createSprite",
+  "createPngCopy",
   "copySpriteGraphic",
   "copySpriteCSS",
   "endClean",
